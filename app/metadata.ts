f387d161ad2013c69f1d97644078e7bf05b32fba
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-09 11:57:54 +0100

    worked on homepage and implemented the Header

---
+++ app/metadata.ts
@@ -0,0 +1,7 @@
+
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Eco Smart Waste",
+  description: "A smart waste management platform leveraging Next.js, TypeScript, TailwindCSS, and Gemini AI. The app features AI-assisted waste verification, a user reward system for eco-friendly actions, real-time waste collection task management, and an interactive leaderboard to encourage community engagement.",
+}
--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -3,6 +3,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require"
+  process.env.DATABASE_URL
 );
 export const db = drizzle(sql, { schema });
