7fdc3c2da2d3226dec1f4cc46503b1514cbb6175
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-12 23:06:44 +0100

    fixed database conection issue by adding connection varibales to next.config.js

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -3,6 +3,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  'postgresql://neondb_owner:npg_h59CBiVAryez@ep-divine-meadow-a8e0b90p-pooler.eastus2.azure.neon.tech/neondb?sslmode=require';
+  process.env.DATABASE_URL
 );
 export const db = drizzle(sql, { schema });
