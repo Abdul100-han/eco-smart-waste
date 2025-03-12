fb6c402ff120a9ce1e9f287807e30e1e3673be9e
Abdul100-han <abdulmuhd857@gmail.com>
2025-03-12 23:01:47 +0100

    updated database conection from .env file to pure string as a result of an error that keep persistig

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,5 +3,5 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Eco Smart Waste",
-  description: "A smart waste management platform leveraging Next.js, TypeScript, TailwindCSS, and Gemini AI. The app features AI-assisted waste verification, a user reward system for eco-friendly actions, real-time waste collection task management, and an interactive leaderboard to encourage community engagement.",
+  description: "A smart AI Powered waste management platform leveraging Next.js, TypeScript, TailwindCSS, and Gemini AI. The app features AI-assisted waste verification, a user reward system for eco-friendly actions, real-time waste collection task management, and an interactive leaderboard to encourage community engagement.",
 }
--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -3,6 +3,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  process.env.DATABASE_URL
+  'postgresql://neondb_owner:npg_h59CBiVAryez@ep-divine-meadow-a8e0b90p-pooler.eastus2.azure.neon.tech/neondb?sslmode=require';
 );
 export const db = drizzle(sql, { schema });
